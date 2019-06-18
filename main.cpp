b0b69630828c74c213ba9798a4fbf1121c001805
Jonathan Moallem <jdmoallem@gmail.com>
2019-06-18 14:01:09 +1000

    Added practise question 0

--- main.cpp
+++ main.cpp
@@ -1,14 +1,36 @@
 #include <iostream>
+using namespace std;
 
+void printArray(int arr[], int size);
 int solution(int A[], int N);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    int A[] = {1, 2};
-    return solution(A, 2);
+    int A[] = {3, 9, 4, 2, 6, -1, 0, 8, 1, 7};
+    std::cout << solution(A, 10) << std::endl;
+    return 0;
+}
+
+void printArray(int arr[], int size) {
+    for(int i = 0; i < size; i++) {
+        std::cout << arr[i] << ' ';
+    }
+    std::cout << std::endl;
 }
 
 int solution(int A[], int N) {
-    return A[N-1];
+    sort(A, A+N);
+    printArray(A, N);
+
+    int idx = 0;
+    int target = 1;
+    int curr = A[idx];
+
+    while(curr <= target) {
+        if (curr > 0) {
+            target++;
+        }
+        curr = A[idx++];
+    }
+    return target;
 }
 
