801ddd8990717f69861bd0ea76032a89a62379d0
Jonathan Moallem <jdmoallem@gmail.com>
2019-06-19 14:46:43 +1000

    Added solution 3

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 //void printArray(int arr[], int size);
 //int solution(int A[], int N);
 //int solution(vector<int> &A);
+int solution(string &A, string &B);
 
 int main() {
 //    int A[] = {3, 9, 4, 2, 6, -1, 0, 8, 1, 7};
@@ -13,6 +14,11 @@ int main() {
 //    static const int arr[] = {1, 4, -1, 3, 2};
 //    vector<int> vec(arr, arr + sizeof(arr) / sizeof(arr[0]));
 //    int sol = solution(vec);
+
+    string A = "rather";
+    string B = "d";
+    int sol = solution(A, B);
+    cout << "Solution: " << sol << endl;
     return 0;
 }
 
@@ -50,12 +56,25 @@ int main() {
 //    return count;
 //}
 
-    while(curr <= target) {
-        if (curr > 0) {
-            target++;
-        }
-        curr = A[idx++];
-    }
-    return target;
-}
+int solution(string &A, string &B) {
+    vector<int> toRemove;
+    for(int i=0; i < A.length(); i++) {
+        auto itr = find(B.begin(), B.end(), A[i]);
+        int idx = itr - B.begin();
 
+        if (idx != B.length()) {
+            B.erase(B.begin() + idx);
+        }
+        else {
+            toRemove.push_back(A[i]);
+        }
+    }
+
+    for(int i=0; i < toRemove.size()-1; i++) {
+        auto itr = find(A.begin(), A.end(), toRemove[i]);
+        int idx = itr - A.begin();
+        A.erase(A.begin() + idx);
+    }
+
+    return B.length() + A.length();
+}
