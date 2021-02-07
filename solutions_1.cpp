41e153077ca575cdf84b900b07fe8d2c8250dd91
Jonathan Moallem <jdmoallem@gmail.com>
2021-02-07 13:19:40 +1100

    Namespaced first solutions set

--- solutions_1.cpp
+++ solutions_1.cpp
@@ -1,79 +1,93 @@
 #include <iostream>
 #include <vector>
-using namespace std;
 
-//void printArray(int arr[], int size);
-//int solution(int A[], int N);
-//int solution(vector<int> &A);
-int solution(string &A, string &B);
 
-void run() {
-    //    int A[] = {3, 9, 4, 2, 6, -1, 0, 8, 1, 7};
-    //    std::cout << solution(A, 10) << std::endl;
+namespace Solutions1
+{
 
-    //    static const int arr[] = {1, 4, -1, 3, 2};
-    //    vector<int> vec(arr, arr + sizeof(arr) / sizeof(arr[0]));
-    //    int sol = solution(vec);
+    using namespace std;
 
-    string A = "rather";
-    string B = "d";
-    int sol = solution(A, B);
-    cout << "Solution: " << sol << endl;
-}
 
-//void printArray(int arr[], int size) {
-//    for(int i = 0; i < size; i++) {
-//        std::cout << arr[i] << ' ';
-//    }
-//    std::cout << std::endl;
-//}
+    //void printArray(int arr[], int size);
+    //int solution(int A[], int N);
+    //int solution(vector<int> &A);
+    int solution(string &A, string &B);
 
-//int solution(int A[], int N) {
-//    sort(A, A+N);
-//    printArray(A, N);
-//
-//    int idx = 1;
-//    int target = 1;
-//    int curr = A[idx];
-//
-//    while(curr <= target) {
-//        if (curr > 0) {
-//            target++;
-//        }
-//        curr = A[idx++];
-//    }
-//    return target;
-//}
+    void run()
+    {
+        //    int A[] = {3, 9, 4, 2, 6, -1, 0, 8, 1, 7};
+        //    std::cout << solution(A, 10) << std::endl;
 
-//int solution(vector<int> &A) {
-//    int current = A[0];
-//    int count = 1;
-//    while(current != -1) {
-//        count++;
-//        current = A[current];
-//    }
-//    return count;
-//}
+        //    static const int arr[] = {1, 4, -1, 3, 2};
+        //    vector<int> vec(arr, arr + sizeof(arr) / sizeof(arr[0]));
+        //    int sol = solution(vec);
 
-int solution(string &A, string &B) {
-    vector<int> toRemove;
-    for(int i=0; i < A.length(); i++) {
-        auto itr = find(B.begin(), B.end(), A[i]);
-        int idx = itr - B.begin();
-
-        if (idx != B.length()) {
-            B.erase(B.begin() + idx);
-        }
-        else {
-            toRemove.push_back(A[i]);
-        }
+        string A = "rather";
+        string B = "d";
+        int sol = solution(A, B);
+        cout << "Solution: " << sol << endl;
     }
 
-    for(int i=0; i < toRemove.size()-1; i++) {
-        auto itr = find(A.begin(), A.end(), toRemove[i]);
-        int idx = itr - A.begin();
-        A.erase(A.begin() + idx);
+    //void printArray(int arr[], int size) {
+    //    for(int i = 0; i < size; i++) {
+    //        std::cout << arr[i] << ' ';
+    //    }
+    //    std::cout << std::endl;
+    //}
+
+    //int solution(int A[], int N) {
+    //    sort(A, A+N);
+    //    printArray(A, N);
+    //
+    //    int idx = 1;
+    //    int target = 1;
+    //    int curr = A[idx];
+    //
+    //    while(curr <= target) {
+    //        if (curr > 0) {
+    //            target++;
+    //        }
+    //        curr = A[idx++];
+    //    }
+    //    return target;
+    //}
+
+    //int solution(vector<int> &A) {
+    //    int current = A[0];
+    //    int count = 1;
+    //    while(current != -1) {
+    //        count++;
+    //        current = A[current];
+    //    }
+    //    return count;
+    //}
+
+    int solution(string &A, string &B)
+    {
+        vector<int> toRemove;
+        for (int i = 0; i < A.length(); i++)
+        {
+            auto itr = find(B.begin(), B.end(), A[i]);
+            int idx = itr - B.begin();
+
+            if (idx != B.length())
+            {
+                B.erase(B.begin() + idx);
+            }
+            else
+            {
+                toRemove.push_back(A[i]);
+            }
+        }
+
+        for (int i = 0; i < toRemove.size() - 1; i++)
+        {
+            auto itr = find(A.begin(), A.end(), toRemove[i]);
+            int idx = itr - A.begin();
+            A.erase(A.begin() + idx);
+        }
+
+        return B.length() + A.length();
     }
 
-    return B.length() + A.length();
 }
